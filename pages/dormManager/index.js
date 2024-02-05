39ef904e2fed27370c67e5bd388ca327899c327a
kasama07 <gasamatrang@gmail.com>
2024-02-06 00:21:19 +0700

    new file:   pages/dormManager/addRoom/addroom.module.css 	modified:   pages/dormManager/addRoom/index.js 	new file:   pages/dormManager/check/check.module.css 	new file:   pages/dormManager/check/index.js 	modified:   pages/dormManager/dorm.module.css 	modified:   pages/dormManager/index.js 	modified:   pages/dormManager/manageRoom/index.js 	new file:   pages/dormManager/manageRoom/manageRoom.module.css 	modified:   pages/dormManager/manageRoom/room/[slug].js 	new file:   pages/dormManager/manageRoom/room/room.module.css 	new file:   public/arrowbutton.png 	new file:   public/error.png 	new file:   public/iconroom.png 	new file:   public/user.png 	new file:   public/users.png

--- pages/dormManager/index.js
+++ pages/dormManager/index.js
@@ -82,7 +82,25 @@ const  sendDataCheck = async()=>{
             <Navbar/>
             <div className={styles.container}>
 
-              {/* ปอทำส่วนนี้นะครับ */}
+                <div className={styles.Boxgrid}>
+                    <Link href= "/dormManager/addRoom" className={styles.Box1}>
+                        <img className={styles.icon} src="/addroom.png" width={100} height={100}></img>
+                        <img className={styles.iconarrowbottom} src="/circle.png" width={20} height={20}></img>
+                        <div className={styles.Text}>เพิ่มห้อง</div>
+                    </Link>
+                    <Link href= "/dormManager/check" className={styles.Box2}>
+                        <img className={styles.icon} src="/checkview.png" width={100} height={100}></img>
+                        <img className={styles.iconarrowbottom} src="/circle.png" width={20} height={20}></img>
+                        <div className={styles.Text}>ตรวจสอบ</div>
+                    </Link>
+                </div>
+                <div className={styles.Box}>
+                    <Link href="/dormManager/manageRoom" className={styles.Box3}>
+                        <img className={styles.iconarrowbottom1} src="/circle.png" width={20} height={20}></img>
+                        <img className={styles.iconarrowbottom2} src="/edit.png" width={100} height={100}></img>
+                        <div className={styles.Textbox}>แก้ไข</div>
+                    </Link>
+                </div>
 
             </div>
             <Footer/>
